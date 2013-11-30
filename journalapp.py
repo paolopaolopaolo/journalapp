e05d26d646391d818bf3f6fd2fea888d37e55e0a
paolopaolopaolo <dpaolomercado@gmail.com>
2013-11-29 17:09:22 -0800

    second attempt to update

--- journalapp.py
+++ journalapp.py
@@ -1,4 +1,4 @@
-import MySQLdb as mdb, getpass as gp, os,sys,pickle,re,datetime as dt
+import MySQLdb as mdb, getpass as gp, os,sys,pickle,re,datetime as dt,subprocess as sp
 from random import randrange
 
 
@@ -24,58 +24,108 @@ class Scrambler():
 			result+=self.__dict__[char]
 		return result
 
+
 def read_journal(db,username):
 	cursor=db.cursor()
-	sqlexec="SELECT * FROM entries WHERE user=\"%s\"" %username
+	sqlexec="SELECT * FROM entries WHERE user=%s ORDER BY day desc;"
 	cursor.execute("USE diary;")
-	cursor.execute(sqlexec)
+	cursor.execute(sqlexec,username)
 	continuer=""
 	item=""
 	while item is not None and continuer=="":
 		item=cursor.fetchone()
 		try:
-			print "\nDay:%s\tTime:%s" % (item[0],item[1])
+			print "\nDay: %s\tTime: %s" % (item[0],item[1])
 			print "\n%s" % item[3]
 		except TypeError:
 			break
 		continuer=raw_input("")
+		if re.search(r"[Pp][Rr][Ii][Nn][Tt]",continuer)!=None:
+			print_entry(item[3])
+
+		if re.search(r"[Dd][Ee][Ll][Ee][Tt][Ee]",continuer)!=None:
+			confirm=raw_input("Are you sure you wish to delete this entry (Y/N)?").upper()
+			if confirm=="Y":
+				try:
+					cursor.execute("DELETE from entries where entry=%s;", item[3])
+				except mdb.ProgrammingError:
+					print "\n\nSQL injection!! Oh no!!!"
+					read_journal(db,username)
+				db.commit()
+			else:
+			  print "Entry not deleted!\n"
+
+		if re.search(r"[Ee][Dd][Ii][Tt]",continuer)!=None:
+			print "Begin edits!\n"
+			day=re.search(r"([\d]+-[\d]+-[\d]+) ([\d]+:[\d]+:[\d]+).",str(dt.datetime.today())).group(1)
+			time=re.search(r"([\d]+-[\d]+-[\d]+) ([\d]+:[\d]+):[\d]+.",str(dt.datetime.today())).group(2)
+			entry=item[3]+"\nEDIT (%s, %s):" % (day,time)
+			while True:
+				entryline=raw_input("")
+				entry+=entryline+"\n"
+				if entryline=="END":
+					print "Ended edits!\n\n\n"
+					break
+			cursor.execute("UPDATE entries SET entry=%s where entry=%s ",(entry[0:-4],item[3]))
+			db.commit()
+
 	cursor.close()
-	print "End of Entries!"
+	print "End of Entries!\n"
 	read_or_write(db,username)
 
+def print_entry(entry):
+	defaultMSG="EMPTY"
+	x=open("temp.txt",'w')
+	x.write(entry)
+	x.close()
+	sp.call([r'C:\Windows\System32\notepad.exe',"temp.txt"])
+	x=open("temp.txt",'w')
+	x.write(defaultMSG)
+	x.close()
+
 def write_journal(db,username):
-	print "Begin writing!"
+	print "Begin writing!\n"
 	this_moment=str(dt.datetime.today())
-	this_indexed_moment=re.search(r"([\d]+-[\d]+-[\d]+) ([\d]+:[\d]+):",this_moment)
+	this_indexed_moment=re.search(r"([\d]+-[\d]+-[\d]+) ([\d]+:[\d]+:[\d]+).",this_moment)
 	day=this_indexed_moment.group(1)
 	time=this_indexed_moment.group(2)
 	cursor=db.cursor()
 	entry=""
-	continuer=""
-	while continuer=="":
+	while True:
 		entryline=raw_input("")
 		entry+=entryline+"\n"
 		if entryline=="END":
 			break
-	sqlexec="INSERT INTO entries VALUES(\"%s\",\"%s\",\"%s\",\"%s\")" %(day,time,username,entry)
-	cursor.execute(sqlexec)
+	try:
+		sqlexec="INSERT INTO entries VALUES(%s,%s,%s,%s)" 
+		cursor.execute(sqlexec,(day,time,username,entry[0:-4]))
+	except mdb.IntegrityError:
+		this_moment=str(dt.datetime.today())
+		this_indexed_moment=re.search(r"([\d]+-[\d]+-[\d]+) ([\d]+:[\d]+:[\d]+).",this_moment)
+		day=this_indexed_moment.group(1)
+		time=this_indexed_moment.group(2)
+		sqlexec="INSERT INTO entries VALUES(%s,%s,%s,%s)" 
+		cursor.execute(sqlexec,(day,time,username,entry[0:-4]))
+
+
 	db.commit()
 	cursor.close()
 	print "Saved Entry!"
 	read_or_write(db,username)
 
 
+
 def read_or_write(db,username):
 	#Read or Write Submenu
-	read_or_write=raw_input("Read or Write? ")
-	if re.search("[Rr][Ee][Aa][Dd]",read_or_write) != None:
+	r_or_w=raw_input("Read or Write?")
+	if re.search("[Rr][Ee][Aa][Dd]",r_or_w) != None:
 		read_journal(db,username)
-	elif re.search("[Ww][Rr][Ii][Tt][Ee]",read_or_write) != None:
+	elif re.search("[Ww][Rr][Ii][Tt][Ee]",r_or_w) != None:
 		write_journal(db,username)
-	elif re.search("[Ee][Xx][Ii][Tt]",read_or_write) != None:
+	elif re.search("[Ee][Xx][Ii][Tt]",r_or_w) != None:
 		print "Goodbye!"
 		sys.exit(0)
-	elif re.search("[Uu][Ss][Ee][Rr]",read_or_write) != None:
+	elif re.search("[Uu][Ss][Ee][Rr]",r_or_w) != None:
 		menu(db)
 	else:
 		print "Error! Try again!"
@@ -94,16 +144,19 @@ def menu(db):
 		pw1=gp.getpass('Password: ')
 		pw2=gp.getpass('Confirm Password: ')
 		if pw1==pw2:
-			print 'Password confirmed! You are now in our system, %s!' % username
 			passcode=Scrambler(username)
 			pw1=passcode(pw1)
-			sqlexec="INSERT INTO users VALUES (\"%s\",\"%s\");" % (username,pw1)
+			sqlexec="INSERT INTO users VALUES (%s,%s);"
 			cursor=db.cursor()
-			cursor.execute(sqlexec)
-			db.commit()
+			try:
+				cursor.execute(sqlexec,(username,pw1))
+				db.commit()
+				print 'Password confirmed! You are now in our system, %s!' % username
+			except mdb.IntegrityError:
+				print "There already exists a user with that handle. Please try again!"
+				menu(db)
 			cursor.close()
 			read_or_write(db,username)
-
 
 		else:
 			print 'Password not confirmed: Try again!'
@@ -135,8 +188,8 @@ def menu(db):
 		x.close()
 		compare=(username,pw)
 		cursor=db.cursor()
-		sqlexec="SELECT * FROM users WHERE user='%s'" %username
-		cursor.execute(sqlexec)
+		sqlexec="SELECT * FROM users WHERE user=%s"
+		cursor.execute(sqlexec,(username))
 		compare2=cursor.fetchall()
 		cursor.close()
 		if compare[1]==compare2[0][1]:
@@ -153,16 +206,13 @@ def menu(db):
 		print "Enter either 'N' or 'R' and then press enter!\nOr type in 'Exit' and then Enter to exit.\n"
 		menu(db)
 
-def main():
-	###Set parameters for connecting to MySQL system
+def set_db():
 	host=raw_input('MySQL Host?: ')
-	if host=="":
-		host="localhost"	
+	
 	user=raw_input('MySQL User?: ')
-	if user=="":
-		user="root"
+	
 	pw=gp.getpass('MySQL Password?: ')
-
+	
 	###Test connection to MySQL system
 	#Test 1: Parameters Correct? If not, start again from beginning of main(). If so, continue.
 	try:
@@ -176,7 +226,6 @@ def main():
 		else:
 			print "Try again!"
 			main()
-
 	#Test 2: Diary exists on MySQL server? Will attempt to bring up Diary database, and open up tables.
 	#IF diary does not exist or does not have the entries or users relations, it will create a new diary db with
 	#those relations.
@@ -200,7 +249,7 @@ def main():
 			cursor.execute('CREATE DATABASE diary;')
 
 		cursor.execute('USE diary;')
-		cursor.execute('CREATE TABLE entries(day varchar(10), time varchar(5), user varchar(20), entry text, primary key(day,time,user))')
+		cursor.execute('CREATE TABLE entries(day varchar(10), time varchar(8), user varchar(20), entry text, primary key(day,time,user))')
 		cursor.execute('CREATE TABLE users(user varchar(20) primary key, password varchar(20))')
 		db.commit()
 		
@@ -209,6 +258,11 @@ def main():
 	#Start menu!
 	cursor.close()
 	menu(db)
+
+
+def main():
+	###Set parameters for connecting to MySQL system
+    set_db()
 
 if __name__=='__main__':
 	main()
